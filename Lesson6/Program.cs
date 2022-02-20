1a4b42b4fd9f93a7a71c782ed77a7fd490e066b4
Ruslan <karuslan@inbox.ru>
2022-02-20 17:39:59 +0300

    Добавьте файлы проекта.

---
+++ Lesson6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Написать консольное приложение Task Manager, которое выводит на экран запущенные процессы и позволяет завершить указанный процесс. 
+ * Предусмотреть возможность завершения процессов с помощью указания его ID или имени процесса. 
+ * В качестве примера можно использовать консольные утилиты Windows tasklist и taskkill.*/
+
+namespace Lesson6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length); // 0
+
+        }
+    }
+}
